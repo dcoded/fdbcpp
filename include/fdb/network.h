8ab11c8aedb8736b21c4a12a9bc46537d4eefeb8
dcoded <dcoded@users.noreply.github.com>
2014-10-19 20:24:11 -0400

    documented and refactored network.h

--- include/fdb/network.h
+++ include/fdb/network.h
@@ -3,32 +3,43 @@
 #define INCLUDE_FDB_NETWORK_H
 
 #include "exception.h"
+#include "async_wait_enabled.h"
 
 #include <system_error>
 #include <string>
 #include <future>
 #include <cassert>
 
+/** @namespace fdb */
 namespace fdb {
-
-    class network {
+    /**
+     * FoundationDB network event loop manager
+     */
+    class network : async_wait_enabled {
     private:
-        int api_version = -1;
-        std::future <fdb_error_t> f_network_;
+        /** Event loop thread future */
+        std::future <fdb_error_t> future;
     public:
-        
+        /**
+         * Starts FDB network thread
+         * 
+         * @param api_version API version
+         */
         explicit network (int api_version = FDB_API_VERSION);
         
+        /** Stops network thread in case of scope loss due to exception */
         ~network ();
 
-        fdb_error_t join ();
+        /** Destroys the network thread, can only be called once per process */
         std::future <fdb_error_t> stop () const;
 
-        bool ok () const;
-        bool err () const;
+        /**
+         * Implementation of polymorphic asyncronous block/wait.
+         * 
+         * @see fdb::async_wait_enabled::wait
+         */
+        virtual void wait ();
     };
-
-
 
     network::network (int api_version) {
 
@@ -39,20 +50,24 @@ namespace fdb {
         if ((error = fdb_setup_network ()) != 0)
             throw fdb::exception (error);
 
-        f_network_ = std::async (std::launch::async, fdb_run_network);
+        future = std::async (std::launch::async, fdb_run_network);
     }
 
     network::~network () {
         stop ();
     }
 
-
     std::future<fdb_error_t> network::stop () const {
         return std::async (std::launch::async, fdb_stop_network);
     }
 
-    fdb_error_t network::join () {
-         return f_network_.get ();
+    void network::wait () {
+        fdb_error_t error;
+
+        if ((error = future.get ()) != 0)
+            throw fdb::exception (error);
+
+        future.wait ();
     }
 }
 #endif
